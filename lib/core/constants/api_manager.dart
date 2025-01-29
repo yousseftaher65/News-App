1e0f2e84eb67c1a96c4ebd912a97334904c368c3
yousseftaher65 <yousseftaher65@gmail.com>
2025-01-29 11:42:39 +0200

    modified to hide api key

--- lib/core/constants/api_manager.dart
+++ lib/core/constants/api_manager.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'config.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_pojo/models/news_response.dart';
 import 'package:news_pojo/models/sources_response.dart';
@@ -7,7 +7,6 @@ import 'package:news_pojo/models/sources_response.dart';
 class ApiManager {
   static const String baseUrl = 'newsapi.org';
   static const String topHeadlines = '/v2/top-headlines/sources';
-  static const String apiKey = '3c5ed2223f90423aa4df21aa3421b5e2';
   static const String everything =  "/v2/everything";
 
   static Future<SourcesResponse> getSources(String categoryName) async {
